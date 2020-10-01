36a084e2d8323353dbc06bc5ebbc9d313b18a961
Scheider <s.scheider@uu.nl>
2020-10-01 18:33:26 +0200

    finished

--- WorkflowSynthesis/run_flowmap.sh
+++ WorkflowSynthesis/run_flowmap.sh
@@ -2,8 +2,9 @@
 ape="APE-1.0.1_patch-executable.jar"
 exec &> flowmap_log.txt
 
-for q in $(seq 7 8); do 	
-	#rm -f "./flowmap/solution${q}/Figures/*"	
+for q in $(seq 4 4); do 	
+	rm -f "./flowmap/solution${q}/Figures/*"	
+	rm -f "./flowmap/solution${q}bench/Figures/*"
     config="./flowmap/ape${q}.configuration"
     java -jar $ape $config   
 	config="./flowmap/ape${q}bench.configuration"
