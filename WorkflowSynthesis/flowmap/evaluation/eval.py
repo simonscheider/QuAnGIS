b5e2f12baeb45c6d903438194ffa62e9c4980570
Scheider <s.scheider@uu.nl>
2020-10-02 13:29:09 +0200

    final

---
+++ WorkflowSynthesis/flowmap/evaluation/eval.py
@@ -0,0 +1,13 @@
+##This script generates an excel sheet for evaluating workflows
+
+import os
+
+def main():
+    workingdir = r"C:\Users\schei008\.matplotlib\Documents\github\QuAnGIS\WorkflowSynthesis\flowmap"
+    print(os.listdir(workingdir))
+
+    #f = open("demofile.txt", "r")
+
+
+if __name__ == '__main__':
+    main()
